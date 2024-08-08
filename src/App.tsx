6b5f23594f9d1cf988adef1fc788d3736de288c7
Asef00 <asefhosseini00@gmail.com>
2024-08-08 17:33:22 +0330

    configure prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function TheHeader() {
   return (
@@ -10,11 +10,11 @@ function TheHeader() {
         <li>Home</li>
       </ul>
     </nav>
-  );
+  )
 }
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -40,7 +40,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
