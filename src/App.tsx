514d01c995f79390035f77bcbb7f654a8cd83d95
Asef00 <asefhosseini00@gmail.com>
2024-09-10 11:22:22 +0330

    feat: showing dynamic content

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const inspiringQuotes = [
+  'The only way to do great work is to love what you do.', // Steve Jobs
+  'Live as if you were to die tomorrow. Learn as if you were to live forever.', // Mahatma Gandhi
+
+  'The greatest glory in living lies not in never falling, but in rising every time we fall.', // Nelson Mandela
+  "Believe you can and you're halfway there.", // Theodore Roosevelt
+
+  'The future belongs to those who believe in the beauty of their dreams.', // Eleanor Roosevelt
+  "Success is not the key to happiness. Happiness is the key to success. And if you love what you're doing, you'll be successful.", // Albert Schweitzer
+  'The only limit to our realization of tomorrow will be our doubts of today.', // Franklin D. Roosevelt
+
+  'You must be the change you wish to see in the world.', // Mahatma Gandhi
+  "Don't watch the clock; be the clock.", // Unknown
+  'The greatest glory in living lies not in never falling, but in rising every time we fall.', // Nelson Mandela
+]
+
+function genRandomInt(max: number) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
 function Header() {
   return (
     <nav>
@@ -33,9 +53,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p className="mt-4">{inspiringQuotes[genRandomInt(10)]}</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
