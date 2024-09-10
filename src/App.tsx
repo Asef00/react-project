48427b5b862f6da6c8a6ad3fe4c835cb91579119
Asef00 <asefhosseini00@gmail.com>
2024-09-10 14:46:26 +0330

    refactor: using destructuring syntax for props

--- src/App.tsx
+++ src/App.tsx
@@ -36,12 +36,12 @@ function Header() {
   )
 }
 
-function CoreConcept(props: CoreConceptProps) {
+function CoreConcept({ img, title, desc }: CoreConceptProps) {
   return (
     <li className="flex flex-col items-center gap-2 text-black bg-white rounded-md p-4">
-      <img width={50} height={50} src={props.img} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.desc}</p>
+      <img width={50} height={50} src={img} alt={title} />
+      <h3>{title}</h3>
+      <p>{desc}</p>
     </li>
   )
 }
